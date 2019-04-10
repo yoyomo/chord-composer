bf76f96aeffd629920f34c843ed771e4e93ae71a
Armando Ortiz <armando@solairo.co.jp>
2019-04-10 20:08:21 +0900

    get state working

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Button, Text, View} from 'react-native';
 
-export default class App extends React.Component {
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+  }
+
+  selectChordBaseNote = () => {
+    this.setState(previousState => {
+      return {isShowingText: !previousState.isShowingText};
+    });
+  };
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Button type="solid" title={display} onPress={this.selectChordBaseNote}>
+        <Text >{display}</Text>
+      </Button>
+    );
+  }
+}
+
+export default class ChordComposer extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Blink text='I love to blink'/>
+        <Blink text='Yes blinking is so great'/>
+        <Blink text='Why did they ever take this out of HTML'/>
+        <Blink text='Look at me look at me look at me'/>
       </View>
     );
   }
@@ -17,5 +43,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  chordBaseNote: {
+    flex: 1,
+    backgroundColor: '#a01355',
+    color: '#fff',
+    display: 'flex',
   },
 });
