41c59f96a2ec1117dd080d208ecc15192792a31e
Armando Ortiz <armando@solairo.co.jp>
2019-05-17 19:29:51 +0900

    use audiocontext

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,53 @@ import React from 'react';
 import './css/tachyons.css';
 
 export interface ClassAndChildren {
-  className?: string, children?: React.ReactNode
+  className?: string,
+  children?: React.ReactNode
 }
 
 interface ChordElementProps extends ClassAndChildren {
   chord: ChordType
 }
 
-class ChordElement extends React.Component<ChordElementProps & ClassAndChildren>{
+class ChordElement extends React.Component<ChordElementProps & ClassAndChildren> {
+
+  audioContext: AudioContext;
 
   render() {
-    return <div className={"bg-light-red w-100px h-100px white dib tc v-mid pointer ma2 br3"}
-                onClick={this.playChord}
-    >
-      <div className="fixed ">
-      {this.props.chord.name}
+    return (
+      <div className={"bg-light-red w-100px h-100px white dib tc v-mid pointer ma2 br3"}
+           onClick={this.playChord}>
+        <div className="fixed ">
+          {this.props.chord.name}
+        </div>
       </div>
-    </div>
-    ;
+    );
+  }
+
+  constructor(props: ChordElementProps) {
+    super(props);
+    this.audioContext = new AudioContext();
   }
 
   playChord = () => {
-    this.props.chord.noteValues.map(noteValue => {
-      let audioContext= new AudioContext();
 
-      let osc1 = audioContext.createOscillator();
+    this.props.chord.noteValues.map(noteValue => {
+
+      let osc1 = this.audioContext.createOscillator();
       osc1.type = 'sine';
       osc1.frequency.value = noteValue;
 
-      var gain = audioContext.createGain();
+      let gain = this.audioContext.createGain();
       osc1.connect(gain);
-      gain.connect(audioContext.destination);
+      gain.connect(this.audioContext.destination);
 
-      var now = audioContext.currentTime;
+      var now = this.audioContext.currentTime;
       gain.gain.setValueAtTime(1, now);
       gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5);
       osc1.start(now);
       osc1.stop(now + 0.5);
     });
-}
+  }
 }
 
 interface ChordType {
@@ -48,13 +56,17 @@ interface ChordType {
   noteValues: number[],
 }
 
+const getAllNotes = () => {
+  
+};
+
 const App: React.FC = () => {
 
   let chords: ChordType[] = [
-    {name: "A", noteValues:[440,554.37,659.25]},
-    {name: "Bm7", noteValues:[493.88]},
-    {name: "C#maj7", noteValues:[554.37]},
-    ];
+    {name: "A", noteValues: [440, 554.37, 659.25]},
+    {name: "Bm7", noteValues: [493.88]},
+    {name: "C#maj7", noteValues: [554.37]},
+  ];
 
   return (
     <div className={"flex flex-row"}>
