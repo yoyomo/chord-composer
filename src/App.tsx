ab1f4f14629561138bc49c89bf44dfa05a50fbe7
Armando Ortiz <armando@solairo.co.jp>
2019-05-17 18:17:22 +0900

    add tachyons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
 import React from 'react';
-import './App.css';
+import './css/tachyons.css';
 
 export interface ClassAndChildren {
   className?: string, children?: React.ReactNode
 }
 
-interface ChordProps extends ClassAndChildren{
+interface ChordElementProps extends ClassAndChildren {
+  chord: ChordType
+}
+
+class ChordElement extends React.Component<ChordElementProps & ClassAndChildren>{
+
+  render() {
+    return <div className={"bg-light-red w-100px h-100px white dib tc v-mid pointer ma2 br3"}
+                onClick={this.playChord}
+    >
+      <div className="fixed ">
+      {this.props.chord.name}
+      </div>
+    </div>
+    ;
+  }
+
+  playChord = () => {
+    this.props.chord.noteValues.map(noteValue => {
+      let audioContext= new AudioContext();
+
+      let osc1 = audioContext.createOscillator();
+      osc1.type = 'sine';
+      osc1.frequency.value = noteValue;
+
+      var gain = audioContext.createGain();
+      osc1.connect(gain);
+      gain.connect(audioContext.destination);
+
+      var now = audioContext.currentTime;
+      gain.gain.setValueAtTime(1, now);
+      gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5);
+      osc1.start(now);
+      osc1.stop(now + 0.5);
+    });
+}
+}
+
+interface ChordType {
   name: string,
   noteValues: number[],
 }
 
-const Chord = (props: ChordProps) => {
-  return <div className={"bg-red w-100px h-100px tc white"}>{props.name}</div>;
-};
-
 const App: React.FC = () => {
+
+  let chords: ChordType[] = [
+    {name: "A", noteValues:[440,554.37,659.25]},
+    {name: "Bm7", noteValues:[493.88]},
+    {name: "C#maj7", noteValues:[554.37]},
+    ];
+
   return (
     <div className={"flex flex-row"}>
-      <Chord name={"A"} noteValues={[400]}/>
-      <Chord name={"Bm7"} noteValues={[400]}/>
-      <Chord name={"C#maj7"} noteValues={[400]}/>
-
+      {chords.map(chord => {
+        return <ChordElement chord={chord}/>
+      })}
     </div>
   );
 };
