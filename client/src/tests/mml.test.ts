a6a6f4b439e21fce1e705950363be4c8daf124a3
Armando <armando.ortiz1@upr.edu>
2019-06-07 09:48:14 +0900

    pass test

--- client/src/tests/mml.test.ts
+++ client/src/tests/mml.test.ts
@@ -2,29 +2,26 @@ import {calculateMML, parseMMLChords} from "../utils/mml";
 import {initialState} from "../state";
 import {ChordType, recomputeChordGrid} from "../reducers/recompute-chord-grid";
 
+const testMML = (chord: ChordType, variation: number, mmlText: string) => {
+  expect(chord.variation).toEqual(variation);
+  expect(calculateMML(chord)).toEqual(mmlText);
+  expect(parseMMLChords(test.state.chordRules,[mmlText])).toEqual([chord]);
+};
+
 let test = {state: initialState};
 it('saves and retrieves identical chords', () => {
   test.state = {...test.state};
   test.state.selectedKeyIndex = 0;
-  test.state.chordGrid = recomputeChordGrid(test.state);
-
-
-
-  test.state.selectedGridChord = test.state.chordGrid[0];
   test.state.showingVariations = {...test.state.showingVariations};
-  test.state.showingVariations[test.state.selectedGridChord.chordRuleIndex] = true;
+  test.state.showingVariations[0] = true;
+
   test.state.chordGrid = recomputeChordGrid(test.state);
 
-  let chords = test.state.chordGrid.slice(0);
-  expect(calculateMML(chords[0])).toEqual("o2[Ac#e]");
-  expect(parseMMLChords(test.state.chordRules,["o2[Ac#e]"])).toEqual([chords[0]]);
+  expect(test.state.chordGrid.length).toBeGreaterThan(test.state.chordRules.length);
 
-  chords = test.state.chordGrid.slice(0,1);
-  expect(calculateMML(chords[0])).toEqual("o2[c#eA]");
-  expect(parseMMLChords(test.state.chordRules,["o2[c#eA]"])).toEqual([chords[0]]);
 
-  chords = test.state.chordGrid.slice(1,2);
-  expect(calculateMML(chords[0])).toEqual("o2[eAc#]");
-  expect(parseMMLChords(test.state.chordRules,["o2[eAc#]"])).toEqual([chords[0]]);
+  testMML(test.state.chordGrid[0],0, "o2[Ac#e]");
+  testMML(test.state.chordGrid[1],1, "o2[c#eA]");
+  testMML(test.state.chordGrid[2],2, "o2[eAc#]");
 
 });
