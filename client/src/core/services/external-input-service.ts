90a03eef95d2582cc7cb6771180a9879d34b4dbc
Armando Ortiz <armando@solairo.co.jp>
2019-11-08 20:01:49 +0900

    display keyboard keys on chord mapper

--- client/src/core/services/external-input-service.ts
+++ client/src/core/services/external-input-service.ts
@@ -1,22 +1,28 @@
-import {Effect, Services} from "./services";
-import {Action} from "../root-reducer";
-import {toggleChordMapperKey} from "../../reducers/chord-mapper-reducer";
+import { Effect, Services } from "./services";
+import { Action } from "../root-reducer";
+import { toggleChordMapperKey } from "../../reducers/chord-mapper-reducer";
 
-export const KeyboardMapperFirstRow =
-  ['q','2','w','e','4','r','5','t','y','7','u','8','i','9','o','p','-','[','=',']','\\'];
-export const KeyboardMapperSecondRow = ['','','','','','','','','','','','','','','','','','','','',
-  'z','s','x','d','c','f','v','b','h','n','j','m',',','l','.',';','/'];
+export const USKeyboardMapperFirstRow =
+  ['q', '2', 'w', 'e', '4', 'r', '5', 't', 'y', '7', 'u', '8', 'i', '9', 'o', 'p', '-', '[', '=', ']', '\\'];
+export const USKeyboardMapperSecondRow = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+  'z', 's', 'x', 'd', 'c', 'f', 'v', 'b', 'h', 'n', 'j', 'm', ',', 'l', '.', ';', '/'];
+
+const KeyboardMapperFirstRow =
+  ['KeyQ', 'Digit2', 'KeyW', 'KeyE', 'Digit4', 'KeyR', 'Digit5', 'KeyT', 'KeyY', 'Digit7', 'KeyU', 'Digit8', 'KeyI', 'Digit9', 'KeyO', 'KeyP', 'Minus', 'BracketLeft', 'Equal', 'BracketRight', 'Backslash'];
+const KeyboardMapperSecondRow = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '',
+  'KeyZ', 'KeyS', 'KeyX', 'KeyD', 'KeyC', 'KeyF', 'KeyV', 'KeyB', 'KeyH', 'KeyN', 'KeyJ', 'KeyM', 'Comma', 'KeyL', 'Period', 'Semicolon', 'Slash'];
 
 export function withExternalInput(dispatch: (action: Action) => void): Services {
 
   document.body.addEventListener('keydown', e => {
-    let keyIndex = KeyboardMapperFirstRow.indexOf(e.key);
+    e.preventDefault();
+    let keyIndex = KeyboardMapperFirstRow.indexOf(e.code);
 
-    if(keyIndex === -1){
-      keyIndex = KeyboardMapperSecondRow.indexOf(e.key);
+    if (keyIndex === -1) {
+      keyIndex = KeyboardMapperSecondRow.indexOf(e.code);
     }
 
-    if(keyIndex !== -1) {
+    if (keyIndex !== -1) {
       dispatch(toggleChordMapperKey(keyIndex));
     }
   });
--- client/src/views/chords/chord-mapper.tsx
+++ client/src/views/chords/chord-mapper.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import {State} from "../../state";
-import {Action} from "../../core/root-reducer";
-import {ChordMapperKeys, toggleChordMapperKey} from "../../reducers/chord-mapper-reducer";
+import { State } from "../../state";
+import { Action } from "../../core/root-reducer";
+import { ChordMapperKeys, toggleChordMapperKey } from "../../reducers/chord-mapper-reducer";
+import { USKeyboardMapperFirstRow, USKeyboardMapperSecondRow } from "../../core/services/external-input-service";
 
 export const SelectedKey = () => {
-  return <div className={"bg-light-red w2 h2 pa2 br-100"}/>
+  return <div className={"bg-light-red w2 h2 pa2 br-100"} />
 };
+
+interface KeyboardKeyProps {
+  i: number
+}
+export const KeyboardKey = (props: KeyboardKeyProps) => {
+  const firstRowKey = USKeyboardMapperFirstRow[props.i] || "";
+  const secondRowKey = USKeyboardMapperSecondRow[props.i] || "";
+  const letter = secondRowKey + ' ' + firstRowKey;
+  return <div>
+    <div className="absolute bg-light-blue h0_125rem top-0 left-0 w-100" />
+    <div className="absolute bg-light-blue pa1 black top-0 right-0 br2 br--bottom br--left">
+      {letter.toUpperCase()}
+    </div>
+  </div>
+}
 
 export function ChordMapper(dispatch: (action: Action) => void) {
 
@@ -19,30 +35,40 @@ export function ChordMapper(dispatch: (action: Action) => void) {
       <div className={"w-100 bg-light-gray dark-gray"}>
         <div className={"overflow-x-auto overflow-y-hidden gpu pb1"}>
           <div className={"db nowrap"}>
-            <div className="bg-gray w2 h3 dib v-mid pointer" onClick={() => dispatcher.toggleChordMapperKey(1)}/>
             {ChordMapperKeys.map((blackKey, i) => {
-              const width = blackKey === "G#" ? 'w3' : 'w3-5';
+              
+              let width = "";
+              if (blackKey === "G#" && i < ChordMapperKeys.length - 2) {
+                width = 'w3';
+              } else if (i === 1) {
+                width = 'w4';
+              } else {
+                width = 'w3-5'
+              }
+
               return blackKey.includes('#') &&
-                  <div key={"black-key-" + i}
-                       className={`bg-gray light-gray tc ${width} h3 dib v-mid pointer pa3 br b--white`}
-                       onClick={() => dispatcher.toggleChordMapperKey(i)}>
-                    {blackKey}
-                    {state.chordMapperKeys[i] && <SelectedKey/>}
-                  </div>
+                <div key={"black-key-" + i}
+                  className={`bg-gray light-gray tc ${width} h3 dib v-mid pointer pa3 br b--white relative`}
+                  onClick={() => dispatcher.toggleChordMapperKey(i)}>
+                  {blackKey}
+                  <KeyboardKey i={i} />
+                  {state.chordMapperKeys[i] && <SelectedKey />}
+                </div>
             })}
           </div>
 
           <div className={"db nowrap"}>
             {ChordMapperKeys.map((whiteKey, i) => {
               return !whiteKey.includes('#') &&
-                  <div key={"white-key-" + i}
-                       className={"bg-white dark-gray w3 h3 dib tc v-mid pointer pa3 bl b--black"}
-                       onClick={() => dispatcher.toggleChordMapperKey(i)}>
-                    {whiteKey}
-                    {state.chordMapperKeys[i] && <SelectedKey/>}
-                  </div>
+                <div key={"white-key-" + i}
+                  className={"bg-white dark-gray w3 h3 dib tc v-mid pointer pa3 bl b--black relative"}
+                  onClick={() => dispatcher.toggleChordMapperKey(i)}>
+                  {whiteKey}
+                  <KeyboardKey i={i} />
+                  {state.chordMapperKeys[i] && <SelectedKey />}
+                </div>
             })}
-            <div className="bg-white w2 h3 dib v-mid pointer" onClick={() => dispatcher.toggleChordMapperKey(ChordMapperKeys.length - 2)}/>
+
 
           </div>
         </div>
