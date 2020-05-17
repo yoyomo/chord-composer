3d01fa033986e5af39dac98caa117d0c2099c74f
Armando <armando.josue.og@gmail.com>
2020-05-17 16:19:54 -0700

    removed create-react-app autogenerated dependencies

--- client/src/react-root.tsx
+++ client/src/react-root.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import {initialState, State} from "./state";
 import {RootPage} from "./views/root-page";
 import {ClassAndChildren} from "./core/reducers";
