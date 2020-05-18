15204407fb5f23eb1b123566d4fb81a8551749d2
Armando <armando.josue.og@gmail.com>
2020-05-17 18:28:41 -0700

    parcel + proxy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    redirect_to "#{ENV['CLIENT_URL']}#{params[:path]}" || 'public/index.html'
+    redirect_to ENV['CLIENT_URL'] ? "#{ENV['CLIENT_URL']}#{params[:path]}" : ""
   end
 end
